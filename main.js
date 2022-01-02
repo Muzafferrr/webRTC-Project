bcb5d418f3ca42c3d9f3f57ad72d71a335e9521e
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2022-01-02 17:38:04 +0300

    asdsad

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import './style.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+import './node_modules/@firebase/app'
+import './node_modules/@firebase/firestore'
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAD2F6e_8ID7DA-ea1anWOZ1Q1Otvywsok",
